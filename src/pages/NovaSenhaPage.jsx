fca4078b79e2e2895cd01d528be74bc29668ce21
Estér Frank <esterschulterserafim@gmail.com>
2026-06-10 20:18:30 -0300

    Luiza2

--- src/pages/NovaSenhaPage.jsx
+++ src/pages/NovaSenhaPage.jsx
@@ -1,5 +1,38 @@
-function NovaSenhaPage() {
-  return <h1>Nova Senha</h1>
-}
+import InputField from '../components/InputField'
+
+<div>
+  <h1>Nova Senha</h1>
+
+  <p>Informe abaixo sua nova senha.</p>
+
+  <label htmlFor="senha">Senha</label>
+  <InputField
+    id="senha"
+    type="password"
+    value={senha}
+    onChange={(e) => setSenha(e.target.value)}
+  />
+
+  <br />
+
+  <label htmlFor="repitasenha">
+    Repita a Senha
+  </label>
+
+  <InputField
+    id="repitasenha"
+    type="password"
+    value={repitaSenha}
+    onChange={(e) => setRepitaSenha(e.target.value)}
+  />
+
+  <br />
+
+  {erro && <p>{erro}</p>}
+
+  <button onClick={handleNovaSenha}>
+    Salvar
+  </button>
+</div>
 
 export default NovaSenhaPage
--- src/pages/RecuperarSenhaPage.jsx
+++ src/pages/RecuperarSenhaPage.jsx
@@ -1,5 +1,44 @@
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import InputField from '../components/InputField'
+
 function RecuperarSenhaPage() {
-  return <h1>Recuperar Senha</h1>
+  const [email, setEmail] = useState('')
+  const [erro, setErro] = useState('')
+
+  const navigate = useNavigate()
+
+  function handleRecuperarSenha() {
+    if (email.trim() === '') {
+      setErro('Digite seu email')
+      return
+    }
+
+    setErro('')
+    navigate('/nova-senha')
+  }
+
+  return (
+    <div>
+      <h1>Recuperar Senha</h1>
+
+      <InputField
+        type="email"
+        placeholder="Digite seu email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <br />
+      <br />
+
+      {erro && <p>{erro}</p>}
+
+      <button onClick={handleRecuperarSenha}>
+        Enviar
+      </button>
+    </div>
+  )
 }
 
 export default RecuperarSenhaPage
